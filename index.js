38aff9489e2da6aa2f3e662d2cd025192c26db3f
Roman Coedo <romancoedo@gmail.com>
2017-08-11 10:16:14 +0200

    Fix parameter name

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import { makeExecutableSchema } from "graphql-tools";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-const server = ({ schema, context = req => ({}), port = 3002, endpoint = "/graphql" }) => {
+const server = ({ schema: { types: typeDefs, resolvers }, context = req => ({}), port = 3002, endpoint = "/graphql" }) => {
   const app = express();
 
   app.use(cors({ origin: (_origin, cb) => cb(null, true), credentials: true }));
   app.use(
     endpoint,
     bodyParser.json(),
-    graphqlExpress(req => ({ schema: makeExecutableSchema(schema), context: context(req) })),
+    graphqlExpress(req => ({ schema: makeExecutableSchema({ typeDefs, resolvers }), context: context(req) })),
   );
 
   const server = app.listen(port, () => console.log(`Running on http://localhost:${server.address().port}${endpoint}`));
